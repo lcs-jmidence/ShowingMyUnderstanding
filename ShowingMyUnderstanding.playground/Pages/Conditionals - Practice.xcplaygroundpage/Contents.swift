d7c004ead5c8bb7fe24520081d7b30d7cdf19fc9
Julio Midence <jmidence@lcs.on.ca>
2017-11-24 21:01:28 -0500

    I  made it

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Practice.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Practice.xcplaygroundpage/Contents.swift
@@ -23,12 +23,19 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 300, height: 300)
+let canvas = Canvas(width: 400, height: 400)
+canvas.fillColor = Color.red
 
 // Generate a random number
 let number = random(from: 0, toButNotIncluding: 2)
 
-// Replace this comment and add your code below...
+if number == 0 {
+    canvas.drawRectangle(centreX: 200, centreY: 200, width: 100, height: 100)
+}
+if number == 1 {
+    canvas.drawEllipse(centreX: 200, centreY: 200, width: 100, height: 100)
+}
+
 
 /*:
  ## Template code
